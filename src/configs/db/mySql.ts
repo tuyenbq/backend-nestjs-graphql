01346d75d810604c6fc6695f7cc022e62f372c61
TuyenBQ <tuyenbq@vitalify.asia>
2023-01-16 11:07:56 +0700

    push code

--- src/configs/db/mySql.ts
+++ src/configs/db/mySql.ts
@@ -9,16 +9,17 @@ require('dotenv').config({ debug: process.env.APP_DEBUG, path: pathEnv });
 const dbConfig: TypeOrmModuleOptions = {
   type: process.env.RDS_TYPE === 'mysql' ? 'mysql' : 'mssql',
   port: Number(process.env.RDS_PORT) || 3306,
-  host: process.env.RDS_HOST,
-  username: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  database: process.env.RDS_DATABASE,
+  host: '35.223.33.250',
+  username: 'sqlserver',
+  password: 'd#r;C_lv&}[M-zL|',
+  database: 'CCAppStagingDB2',
   entities: [path.join(__dirname, '../..', '/**/*.entity{.ts,.js}')],
   logging: process.env.NODE_ENV === 'e2e' ? ['info', 'log'] : 'all',
   connectionTimeout: Number(process.env.RDS_TIMEOUT) || 2147483647,
   requestTimeout: Number(process.env.RDS_TIMEOUT) || 2147483647,
   options: {
     cancelTimeout: Number(process.env.RDS_TIMEOUT) || 2147483647,
+    encrypt: false,
   },
   synchronize: false, // Don't use it on production, It will sync entity and modify your database to match with entity
 };
