976796c4c837b0eb64967886ce5fee0f1d1e5d47
TuyenBQ <tuyenbq@vitalify.asia>
2023-01-16 15:37:08 +0700

    Update mySql.ts
    fasqdsdas

--- src/configs/db/mySql.ts
+++ src/configs/db/mySql.ts
@@ -7,19 +7,18 @@ const pathEnv =
 // eslint-disable-next-line
 require('dotenv').config({ debug: process.env.APP_DEBUG, path: pathEnv });
 const dbConfig: TypeOrmModuleOptions = {
-  type: 'mssql',
+  type: process.env.RDS_TYPE === 'mysql' ? 'mysql' : 'mssql',
   port: Number(process.env.RDS_PORT) || 1433,
-  host: '35.223.33.250',
-  username: 'sqlserver',
-  password: 'd#r;C_lv&}[M-zL|',
-  database: 'CCAppStagingDB2',
+  host: process.env.RDS_HOST,
+  username: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DATABASE,
   entities: [path.join(__dirname, '../..', '/**/*.entity{.ts,.js}')],
   logging: process.env.NODE_ENV === 'e2e' ? ['info', 'log'] : 'all',
   connectionTimeout: Number(process.env.RDS_TIMEOUT) || 2147483647,
   requestTimeout: Number(process.env.RDS_TIMEOUT) || 2147483647,
   options: {
     cancelTimeout: Number(process.env.RDS_TIMEOUT) || 2147483647,
-    encrypt: false,
   },
   synchronize: false, // Don't use it on production, It will sync entity and modify your database to match with entity
 };
